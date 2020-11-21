9766577863f20e72b51bdf2773a3b193fff0d3dc
mia-vrdoljak <miavrdoljak2@gmail.com>
2020-11-21 16:57:15 +0100

    Ovo je inicijalni commit

---
+++ Internship-1-Git/Internship-1-Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Internship_1_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
